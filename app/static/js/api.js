9ca8fd37dc7d66adf5ef776516aad577efe4e498
leo-frison <leonart_crady205@hotmail.com>
2018-05-15 21:53:16 -0300

    Modifico detalles y agrego boton de borrar

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -21,9 +21,43 @@ const API = (function () {
                 return r.json();
             });
     }
-  
+
+        function deleteProduct(orderId, productId) { /*Funcion que elimina producto de la DB*/
+        return fetch(`/order/${ orderId }/product/${ productId }`,
+            {
+                method: 'DELETE'
+            }
+        )
+        
+        console.log('borro')
+    }
+
+    /**
+     * Agrega un producto a una orden
+     **/
+    function addProduct(orderId, product, quantity) {
+        const data = JSON.stringify({ quantity: quantity, product: product })
+
+        return fetch(`/order/${ orderId }/product`,
+            {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: data
+            }
+        ).then(function toJson(r) {
+            return r.json();
+        });
+    }
+
+
+
     return {
         getOrder,
         getProducts,
-      }
+        addProduct,
+        deleteProduct
+    }
 })()
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -3,10 +3,13 @@
 
     // Estado de la aplicacion
     const state = {
+        products: API.getProducts(),
         selectedProduct: null,
         quantity: 0,
+        order: API.getOrder()
     }
-
+     
+    const refs = {}
     /**
      * Actualiza el valor del precio total
      **/
@@ -14,6 +17,29 @@
         const totalPrice = state.selectedProduct.price * state.quantity;
         $totalPrice.innerHTML = `Precio total: $ ${totalPrice}`
     }
+
+     /**
+     * Agrega un producto a una orden
+     *
+     **/
+    function onAddProduct() {
+        if(state.quantity >= 1){                //Si es una cantidad válida, acepta los valores y los carga
+            API.addProduct(1, state.selectedProduct, state.quantity)
+            .then(function (r) {
+                if (r.error) {
+                    console.error(r.error);
+                } else {
+                    API.getOrder().then(function (data) {
+                        refs.table.update(data);
+                    });
+
+                    refs.modal.close();
+                }
+            });
+        }else{                                  //Si es nulo o negativo, muestra mensaje de error
+            alert("Error. No puede ingresarse una cantidad negativa o nula.");
+        }
+}
 
     /**
      * Inicializa la aplicacion

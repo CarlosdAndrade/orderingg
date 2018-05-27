f3fb1f0751309ad122a99935f7c3775b79a54174
nahuel baez <naubaez@gmail.com>
2018-05-27 10:57:19 -0300

    test con selenium, ej 2)c) MEJORAR SOLUCION

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -8,42 +8,70 @@ from selenium import webdriver
 from app import create_app, db
 from app.models import Product, Order, OrderProduct
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+from werkzeug.serving import make_server
 
 class Ordering(unittest.TestCase):
-
-    #se crea la base de datos de test
-    def create_app(self):
-        app = create_app()
-        app.config.update(
+    # Creamos la base de datos de test
+    def setUp(self):
+        self.app = create_app()
+        self.app.config.update(
             SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'test.db'),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
             TESTING=True
         )
 
-        return app
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
-    # Creamos la base de datos de test
-    def setUp(self):
-        self.baseURL = self.get_server_url()
+        self.baseURL = 'http://localhost:5000'
 
         db.session.commit()
         db.drop_all()
         db.create_all()
 
-        self.driver = webdriver.Chrome()
-"""
+        self.t = threading.Thread(target=self.app.run)
+        self.t.start()
+
+        time.sleep(1)
+
+        self.driver = webdriver.Firefox()
+
     def test_title(self):
         driver = self.driver
         driver.get(self.baseURL)
-        self.assertIn("Ordering", driver.title)
+        add_product_button = driver.find_element_by_xpath('/html/body/main/div[1]/div/button')
+        add_product_button.click()
+        modal = driver.find_element_by_id('modal')
+        assert modal.is_displayed(), "El modal no esta visible"
 
+    
+#         ACTIVIDAD 3 punto 2, incsiso c
+    
+    def test_selenium_cant_negativa(self):
+        driver = self.driver
+        driver.get(self.baseURL)
+
+        boton_agregar = driver.find_element_by_xpath('/html/body/main/div[1]/div/button')
+        boton_agregar.click()
+
+        cant =  driver.find_element_by_xpath('//*[@id="quantity"]')
+        cerrar_modal = driver.find_element_by_xpath('//*[@id="modal"]/div[2]/footer/button[3]')
+
+        time.sleep(4)
+        cerrar_modal.click()
+
+        self.assertTrue(cant >= 1 , "valor negativo")
+
+        
     def tearDown(self):
+        self.driver.get('http://localhost:5000/shutdown')
+
         db.session.remove()
         db.drop_all()
         self.driver.close()
-"""
+        self.app_context.pop()
+    
 if __name__ == "__main__":
     unittest.main()

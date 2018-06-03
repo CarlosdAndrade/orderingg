9361ee2c7809ede9bc3e7440718f5ca109aa268a
leo-frison <leonart_crady205@hotmail.com>
2018-06-03 04:15:10 -0300

    Subo los test unitarios explicados y testeados mas el integral del modal y un opcional

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -46,17 +46,101 @@ class Ordering(unittest.TestCase):
         modal = driver.find_element_by_id('modal')
         assert modal.is_displayed(), "El modal no esta visible"
 
+     
 
-     
-     
+     #-------------ACTIVIDAD 3 - punto 1)  b) -------------------------------------------------------
+    
+    def test_InfoModalEditar(self):
+        #Para que este test  funcione debe haber productos cargados sino fallara ya que sin productos 
+        #no se puede editar
+        #Genero una orden
+        o = Order(id= 1)
+        db.session.add(o)
+        #Genero un producto
+        p = Product(id= 1, name= 'vaso', price= 500)
+        db.session.add(p)
+        #Relaciono orden y producto
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 1, product= p)
+        db.session.add(orderProduct)
+        db.session.commit()
+        #Me traigo la base
+        driver = self.driver
+        driver.get(self.baseURL)
+        #tiempo de espera de 4 segundos
+        time.sleep(4)
+        #busco el boton de edicion que esta en la fila de productos junto al boton de eliminar y lo clickeo
+        boton_editar = driver.find_element_by_xpath('/html/body/main/div[2]/div/table/tbody/tr[1]/td[6]/button[1]')
+        boton_editar.click()
+        #busco las referencias que tiene el producto tanto selecion como la cantidad 
+        producto = driver.find_element_by_xpath('//*[@id="select-prod"]')
+        cantidad = driver.find_element_by_xpath('//*[@id="quantity"]')
+        #tomo el atributo nombre
+        value_prod = producto.get_attribute("value")
+        #tomo el atributo de su cantidad
+        value_cant = cantidad.get_attribute("value")
+        #identifico el boton que cierra el modal y lo clickeo
+        boton_cerrar_modal = driver.find_element_by_xpath('//*[@id="modal"]/div[2]/footer/button[3]')
+        time.sleep(5)
+        boton_cerrar_modal.click()
+        #finalmente evaluo si es verdero que tanto el valor como la cantidad sean distintas de vacio o 
+        # devuelve que no tienen informacion cargada
+        self.assertTrue(value_prod != "", "No tiene informacion")
+        self.assertTrue(value_cant != "", "No tiene informacion")
+
+    
+    #ACTIVIDAD OPCIONAL ASIGNADA 2)b)
+
+    def test_Notificacion_Aparece(self):
+        
+        #Genero producto
+        p = Product(id=1, name = 'Mesa', price = 500)
+        db.session.add(p)
+        
+        #Genero una order
+        o = Order(id=1)
+        db.session.add(o)
+        
+        #Asigno la orden al producto creado
+        op = OrderProduct(product=p, quantity=1)
+        db.session.add(op)
+        db.session.commit()
+
+        #Me traigo la base
+        driver = self.driver
+        driver.get(self.baseURL)
+        #Tomo la ruta del boton agregar producto copiando su xpath
+        agregar = driver.find_element_by_xpath("/html/body/main/div[1]/div/button").click()
+        #Busco el elemento seleccionar producto 
+        producto = driver.find_element_by_id("select-prod")
+        #Selecciono el elemento visible Mesa en este caso ya q es el que quiero repetir guardar
+        #la mesa para que me salte la notificacion
+        producto.select_by_visible_text('Mesa')
+        #Busco el elemento cantidad
+        cantidad = driver.find_element_by_id("quantity")
+        #Edito la cantidad 
+        cantidad.SendKeys('1')
+        #Busco el elemento boton guardar y clickeo
+        guardar = driver.find_element_by_id("save-button").click()
+        #busco el elemento notificacion y lo muestro
+        noti = driver.find_element_by_id('noti').is_displayed()
+        #verifico que aparesca la notificacion
+        self.assertEqual(noti, True,"No pararece la notifacion")
+        #LAS SIGUIENTES SON OPCIONES QUE SE ME OCURRIERON PARA QUE NO ME FALLE EL TEST:
+        #self.assertEqual(noti, False,"No pararece la notifacion")
+        #self.assertEqual(len(noti),"No se pueden agregar productos con nombre repetido", "No hay productos")
+        #self.assertTrue(noti=="No se pueden agregar productos con nombre repetido","No hay productos")
+
+
+
         
     def tearDown(self):
         self.driver.get('http://localhost:5000/shutdown')
-
         db.session.remove()
         db.drop_all()
         self.driver.close()
         self.app_context.pop()
 
+  
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main() 
+
--- test/test_unit.py
+++ test/test_unit.py
@@ -49,5 +49,68 @@ class OrderingTestCase(TestCase): # Creacion de una clase que contiene todos nue
         resp = self.client.post('/product', data=json.dumps(data), content_type='application/json')
 
 
+
+
+     #--------------------ACTIVIDAD 3 - punto 1) a) ----------------------------------------------------------
+
+    def test_put(self):
+        
+        #Creo el producto
+        p = Product(id= 1, name= 'vaso', price= 500)
+        db.session.add(p)
+
+        #Creo la orden
+        o = Order(id= 1)
+        db.session.add(o)   
+        
+        #Relaciono la orden al producto creado
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 1, product= p)
+        db.session.add(orderProduct)
+        db.session.commit()
+        #Este sera el dato que voy a modificar con canidad 10 en el producto que acabo de cargar
+        data = {
+            'quantity': 5
+        }
+        #Invoco el metodo put en el documento de prueba json en el argumento orden 1 con producto 1
+        self.client.put('order/1/product/1', data=json.dumps(data), content_type='application/json')
+        arg = 1,1
+        #Me traigo este argumento
+        prod = OrderProduct.query.get(arg)
+        #Verifico q la cantidad del producto es la modificada en este caso 5
+        self.assertTrue(prod.quantity == 5, "Fallo el metodo PUT")
+        #Me traigo todos los productos y verifico que haya un solo producto en lista
+        p = Product.query.all()
+        self.assertEqual(len(p), 1, "No hay productos")
+     
+
+     #--------------------ACTIVIDAD 3 - punto 1) c) ------------------------------------------------
+    
+    def test_OrderPrice(self): 
+        
+        #Creo el producto
+        p = Product(id= 1, name= 'vaso', price= 500)
+        db.session.add(p)
+        
+        #Creo la orden
+        o = Order(id= 1)
+        db.session.add(o)
+        
+        #Creo la relacion entre el producto y la orden
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 4, product= p)
+        db.session.add(orderProduct)
+        db.session.commit()
+        
+        #me traigo la order creada y verifico que el precio se haya calculado bien
+        orden= Order.query.get(1)
+        totalPrice = orden.orderPrice
+        self.assertEqual(2000, totalPrice, "El precio total no se calcula bien")
+        #En este caso me pasa porque el vaso de 500 mangos y la cantidad 4 me da 2000 p   
+
+
+ # El  if __name__ == '__main__': sirve para que mi fichero test_unit.py se ejecuten, desde la terminal de forma automatica, todos los tests creados
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
